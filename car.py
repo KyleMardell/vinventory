5e1fa52a3b4184828b7070b176d4f51dc5bae903
Kyle Mardell <151871777+KyleMardell@users.noreply.github.com>
2024-05-26 20:34:28 +0100

    Begin to make Car class

--- car.py
+++ car.py
@@ -23,4 +23,5 @@ class Car:
     def display_info(self):
         table = PrettyTable()
         table.field_names = ["ID", "Make", "Model", "Year", "Milage", "Engine", "Colour", "Status", "Price", "Cost", "Repairs"]
-        table.add_row([self.id, self.make, self.model, self.year, self.milage, self.engine, self.colour, self.status, self.price, self.cost, self.repairs])
+        table.add_row([self.id, self.make, self.model, self.year, self.milage, self.engine, self.colour, self.status, self.price, self.cost, self.repairs]
+        print(table))
